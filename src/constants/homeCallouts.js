b059b423f2c0859e60c0d641d2d22627dd8657e0
rameshelamathi <rameshelamathi@gmail.com>
2019-03-14 17:22:34 +0530

    - Callouts content shortened

--- src/constants/homeCallouts.js
+++ src/constants/homeCallouts.js
@@ -9,37 +9,37 @@ const HOME_CALLOUTS = [
     {
         IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/follow-up-emails.png`,
         TITLE: 'Follow-up Emails',
-        COPY: 'Using the powerful segmentation, send smarter emails to build your brand. Ask customers for a feedback, a review, or just say hello to establish a lasting relationship.'
+        COPY: 'Send a series of follow-up emails and nurture your customers to build a lasting relationship'
     },
     {
         IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/win-back-inactive-customers.png`,
-        TITLE: 'Win-back Inactive Customers',
-        COPY: 'Bring back inactive customers with a series of messages designed to drive repeat purchases. Send a coupon code or a special offer if you did not see someone back in 60 days.'
+        TITLE: 'Win-Back Inactive Customers',
+        COPY: 'Bring back inactive customers with a series of messages designed to drive repeat purchases.'
     },
     {
         IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/product-recommendations.png`,
         TITLE: 'Product Recommendations',
-        COPY: 'Automatically boost your sales by suggesting related products based on the purchase history of the customers. Example: You can suggest "Socks" to those who bought Shoes'
+        COPY: 'Automatically boost your sales by suggesting related products based on the purchase history of the customers.'
     },
     {
         IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/360-degree-view-of-customers.png`,
         TITLE: 'Welcome Emails',
-        COPY: 'Create a great first impression and start building a brand loyalty right from their sign-up. A personalised welcome email can build a better brand image'
+        COPY: 'Create a great first impression and start building a brand loyalty right from their sign-up.'
     },
     {
         IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/powerful-segmentation.png`,
         TITLE: 'On-site messages',
-        COPY: 'Engage and convert your visitors into customers with on-site popups / slide-ins / action bars. Trigger them based on their behavior, visit to a product page or if they try to exit.'
+        COPY: 'Engage and convert your visitors into customers with on-site popups / slide-ins / action bars.'
     },
     {
         IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/drag-and-drop-visual-email-builder.png`,
         TITLE: 'Drag and drop visual email builder',
-        COPY: 'Create beautiful emails that reflect your brand with our intuitive drag-and-drop visual email builder. Use short codes to add dynamic data like their names.'
+        COPY: 'Create beautiful emails that reflect your brand with our intuitive drag-and-drop visual email builder.'
     },
     {
         IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/supports-your-favourite-ecommerce-platform-woocommerce.png`,
         TITLE: 'Powerful Segmentation',
-        COPY: 'Segment your customers based on their order history, total spending, purchase behaviour, products bought, abandoned carts, location and more.'
+        COPY: 'Segment your customers based on their purchase history, total spending, abandoned carts, location and more.'
     },
 ];
 
