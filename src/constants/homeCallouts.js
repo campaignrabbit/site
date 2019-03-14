e9bf830c491986544796e476176298b8e6511eed
rameshelamathi <rameshelamathi@gmail.com>
2019-03-14 12:52:21 +0530

    - Images are now externally hosted

--- src/constants/homeCallouts.js
+++ src/constants/homeCallouts.js
@@ -1,52 +1,43 @@
 import React from 'react';
-import customerView from '../images/home-icons/360-degree-view-of-customers.png';
-import abandCart from '../images/home-icons/abandoned-cart-recovery.png';
-import dragDrop from '../images/home-icons/drag-and-drop-visual-email-builder.png';
-import followEmail from '../images/home-icons/follow-up-emails.png';
-import productRec from '../images/home-icons/product-recommendations.png';
-import winBack from '../images/home-icons/win-back-inactive-customers.png';
-import segment from '../images/home-icons/powerful-segmentation.png';
-import platform from '../images/home-icons/supports-your-favourite-ecommerce-platform-woocommerce.png';
-
 
 const HOME2_CALLOUTS = [
     {
-        IMAGE: abandCart,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/abandoned-cart-recovery.png`,
         TITLE: 'Recover abandoned carts',
         COPY: 'Automatically remind shoppers who have abandoned their cart and recover them. Engage them when they try to exit a product page / cart / checkout with a popup'
     },
     {
-        IMAGE: followEmail,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/follow-up-emails.png`,
         TITLE: 'Follow-up Emails',
         COPY: 'Using the powerful segmentation, send smarter emails to build your brand. Ask customers for a feedback, a review, or just say hello to establish a lasting relationship.'
     },
     {
-        IMAGE: winBack,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/win-back-inactive-customers.png`,
         TITLE: 'Win-back Inactive Customers',
         COPY: 'Bring back inactive customers with a series of messages designed to drive repeat purchases. Send a coupon code or a special offer if you did not see someone back in 60 days.'
     },
     {
-        IMAGE: productRec,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/product-recommendations.png`,
         TITLE: 'Product Recommendations',
         COPY: 'Automatically boost your sales by suggesting related products based on the purchase history of the customers. Example: You can suggest "Socks" to those who bought Shoes'
     },
     {
-        IMAGE: customerView,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/360-degree-view-of-customers.png`,
         TITLE: 'Welcome Emails',
         COPY: 'Create a great first impression and start building a brand loyalty right from their sign-up. A personalised welcome email can build a better brand image'
     },
     {
-        IMAGE: platform,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/powerful-segmentation.png`,
         TITLE: 'On-site messages',
         COPY: 'Engage and convert your visitors into customers with on-site popups / slide-ins / action bars. Trigger them based on their behavior, visit to a product page or if they try to exit.'
     },
     {
-        IMAGE: dragDrop,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/drag-and-drop-visual-email-builder.png`,
         TITLE: 'Drag and drop visual email builder',
         COPY: 'Create beautiful emails that reflect your brand with our intuitive drag-and-drop visual email builder. Use short codes to add dynamic data like their names.'
     },
     {
-        IMAGE: segment,
+        IMAGE: `${process.env.CR_MEDIA_URL}/images/home-icons/supports-your-favourite-ecommerce-platform-woocommerce.png`,
         TITLE: 'Powerful Segmentation',
         COPY: 'Segment your customers based on their order history, total spending, purchase behaviour, products bought, abandoned carts, location and more.'
     },
