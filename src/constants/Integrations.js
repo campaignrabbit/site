9206d5aa9d5184ca17a54905809f79eb88045d14
rameshelamathi <rameshelamathi@gmail.com>
2019-03-19 16:54:34 +0530

    - Slightly re-done the integrations page

--- src/constants/Integrations.js
+++ src/constants/Integrations.js
@@ -3,23 +3,27 @@ import React from 'react';
 const INTEGRATIONS = [
     {
         LOGO: `${process.env.CR_MEDIA_URL}/images/integrations/logo-woocommerce.png`,
+        ALT: 'WooCommerce Integration for Campaignrabbit',
         TITLE: 'WooCommerce',
-        DESC: 'Download and install the plugin for WooCommerce and set it up in your WordPress site.',
+        DESC: 'Connect your WooCommerce store with Campaignrabbit by installing our free plugin',
         DLINK: 'https://github.com/campaignrabbit/campaignrabbit-integration-for-woocommerce/releases/download/stable/plugin-campaignrabbit-integration-for-woocommerce.zip'
     },
     {
         LOGO: `${process.env.CR_MEDIA_URL}/images/integrations/j2store_logo.png`,
+        ALT: 'J2Store Integration for Campaignrabbit',
         TITLE: 'J2Store',
-        DESC: 'Download and install the plugin for J2Store and set it up in your Joomla site.',
-        DLINK: 'https://github.com/campaignrabbit/j2store_campaignrabbit/releases/download/stable/pkg_app_campaignrabbit.zip'
+        DESC: 'J2Store comes with a pre-built integration with Campaignrabbit. Connect your store to get started',
+        DLINK: 'https://www.j2store.org/download.html'
     },
     {
         LOGO: `${process.env.CR_MEDIA_URL}/images/integrations/magento.png`,
+        ALT: 'Magento Integration for Campaignrabbit',
         TITLE: 'Magento',
         DESC: 'Coming soon..',
     },
     {
         LOGO: `${process.env.CR_MEDIA_URL}/images/integrations/shopify.png`,
+        ALT: 'Shopify app for Campaignrabbit',
         TITLE: 'Shopify',
         DESC: 'Coming soon..',
     },
