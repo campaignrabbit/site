c084d0829483c1497ad8e1847c176ac9e0f1e9f5
ShobanaEswaran <eswaranshobana@gmail.com>
2019-03-27 13:26:45 +0530

    home page changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { FaPlay, FaTimes } from 'react-icons/fa';
 import Layout from '../components/layout'
-import SEO from '../components/seo'
+import MetaTags from '../components/Metatags'
 import $ from 'jquery'
 import SignUpForm from '../components/SignUpForm'
 
@@ -19,17 +19,20 @@ class IndexPage extends React.Component {
     render(){
         return (
             <Layout>
-                <SEO title="Campaignrabbit - E-Commerce Marketing Automation for WooCommerce and Shopify"
-                     keywords={['ecommerce marketing','marketing automation', 'email marketing', 'WooCommerce Email Marketing', 'Shopify Email Marketing']}/>
-
+                <MetaTags
+                    title="Campaignrabbit - Email Marketing Automation for WooCommerce and Shopify"
+                    description="Campaignrabbit is an eCommerce Marketing Automation platform to send personalised, data-drive emails and popups on autopilot. Try Campaignrabbit for FREE today!"
+                    thumbnail={`${process.env.CR_MEDIA_URL}/images/campaignrabbit-logo.png`}
+                    url="https://wwww.campaignrabbit.com"
+                />
                 <div id="home" className="home">
                     <div className="container-m text-center">
                         <div className="hero-content wow fadeIn">
                             <div className="row">
                                 <div className="col-sm-12">
-                                    <h1>Sell more by sending <span className="highlight">personalised, highly relevant emails</span> and  <span className="highlight">on-site messages</span></h1>
-                                    <p>Campaignrabbit offers tools to <span className="underline">engage and convert visitors into customers smartly</span> for growth-focused eCommerce businesses.
-                                        </p>
+                                    <h1>Sell more by sending personalised, highly relevant emails and popups</h1>
+                                    <p>Campaignrabbit offers Email Marketing and Popups for growth-focused eCommerce businesses. <br /><span className="underline">Engage and convert visitors into customers smartly</span>
+                                    </p>
                                     <SignUpForm user_mail_id={false}
                                       btnText="Get started for FREE" btnClass="btn-action" description="Join 1000+ businesses and put your marketing on autopilot today."
                                       note="No credit card required" />
