5ee8495549e476cc57d945ce3c9962922124c46b
ShobanaEswaran <eswaranshobana@gmail.com>
2019-03-28 17:32:49 +0530

    - added faq to pricing page

--- src/constants/homeFeatures.js
+++ src/constants/homeFeatures.js
@@ -5,7 +5,7 @@ import CALLOUTS from '../constants/homeCallouts';
 class homeFeature extends React.Component {
     static renderCallouts() {
         return CALLOUTS.map((card) => {
-            const cardClass = `col-md-3 col-sm-6`;
+            const cardClass = `col-lg-3 col-md-4 col-sm-6`;
             return (
                 <div className={cardClass} key={card.TITLE}>
                     <div className="card text-center feature-list">
